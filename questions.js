e430ef6436cd7b86dfc0a917b92f79a3554c16f7
mato122 <80591816+mato122@users.noreply.github.com>
2024-01-14 11:32:06 -0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "¿What is the definition of cute?",
+    answer:"All of the above",
+    options: [
+      "(esp. of something or someone small or young) charming and attractive",
+      "Something described as cute is attractive and pleasant",
+      "too carefully designed to get approval or appear attractive, and therefore seeming dishonest",
+      "All of the above"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which of the animals before is found out to be the cutest",
+    answer: "Red Panda",
+    options: [
+      "Hedgehog",
+      "Red Panda",
+      "Koala",
+      "Axolotl"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Who does the internet say is the 2nd cutest pokemon?",
+    answer: "Eevee",
+    options: [
+      "Eevee",
+      "Teddiursa",
+      "Shaymin",
+      "Squirtle"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which of the following song tittle is actually a cute song (upbeat, pop)",
+    answer: "Ferris Wheel",
+    options: [
+      "You Would Never",
+      "TERRIBLE THINGS",
+      "Ferris Wheel",
+      "Twister"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Is there really a way to be cute?",
+    answer: "Not really, it just depends on the person",
+    options: [
+      "Not really, it just depends on the person",
+      "Yes obviously, all pink and glitter....",
+      "Yeah spiders and gloomy places are cute. ",
+      "Both B and C"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
